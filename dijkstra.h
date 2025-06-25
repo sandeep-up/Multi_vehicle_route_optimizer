8b6e6417b50ec0b494c20907bc6f0d8a9ea0cc9d
Sandeep Upadhyay <157040731+sandeep-up@users.noreply.github.com>
2025-06-25 18:09:42 +0530

    Add dijkstra.h: declaration of findOptimalRoute()

---
+++ dijkstra.h
@@ -0,0 +1,9 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+#include <string>
+void findOptimalRoute(const std::string& source,
+                      const std::string& destination,
+                      const std::string& vehicle);
+
+#endif
